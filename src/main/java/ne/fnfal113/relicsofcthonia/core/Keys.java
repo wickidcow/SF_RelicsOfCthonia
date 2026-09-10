7cba2ec185f5fa6b00223203e0617c87cb8dc039
wickidcow <jaywago@gmail.com>
2026-09-10 19:23:24 -0400

    fix: track temporary piglin barter material

--- src/main/java/ne/fnfal113/relicsofcthonia/core/Keys.java
+++ src/main/java/ne/fnfal113/relicsofcthonia/core/Keys.java
@@ -8,6 +8,7 @@ public class Keys {
 
     public static final NamespacedKey SPAWNER_MOB = createKey("relic_spawned_mob");
     public static final NamespacedKey CURRENTLY_TRADING_PLAYER = createKey("currently_trading_player");
+    public static final NamespacedKey CURRENTLY_TRADING_MATERIAL = createKey("currently_trading_material");
     public static final NamespacedKey RELIC_CONDITION = createKey("relic_condition");
     public static final NamespacedKey RELIC_CONDITION_QUOTA = createKey("condition_quota");
 
